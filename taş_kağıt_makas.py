17a058b5430761463df34d6b7319ef2890b39dc9
Mehmethan AYTEKELİ <mehmethanaytekeli@gmail.com>
2022-12-19 00:05:51 +0300

    Create taş_kağıt_makas.py

---
+++ taş_kağıt_makas.py
@@ -0,0 +1,35 @@
+import random
+
+print("""Taş Kağıt Makas Oyunu
+************************************""")
+
+
+import random
+
+liste =  ['taş','kağıt','makas']
+
+a = input("Hamle Yapın :")
+
+rastgeledeger = random.choice(liste)
+
+print("Botun Hamlesi:{}".format(rastgeledeger))
+
+if rastgeledeger == a :
+    print(("Berabere !"))
+elif rastgeledeger == "taş" and a == "kağıt" :
+    print("Kazandınız !")
+elif rastgeledeger == "kağıt" and a == "Makas" :
+    print("Kazandınız !")
+elif rastgeledeger == "makas" and a == "taş" :
+    print("Kazandınız !")
+else :
+   print("Mağlup Oldunuz !")
+
+
+
+
+
+
+
+
+
